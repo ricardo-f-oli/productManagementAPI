1f13f74689804e46e95de1856f00b39ee92871f4
Ricardo Oliveira <ricardo.fer.oliveira964@gmail.com>
2025-01-12 13:03:08 -0300

    Starting application development

---
+++ src/main/java/com/project/productManagementAPI/ProductManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.productManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementApiApplication.class, args);
+	}
+
+}
